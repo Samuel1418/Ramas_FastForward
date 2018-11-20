aba1e71c2fc2bb2f67317ff812823dcfa1456b4c
Samuel1418 <sgomezpintos@danielcastelao.org>
2018-11-20 08:54:03 +0100

    Segundo commit

--- pruebaramasfastforward/PruebaRamasFastForward.java
+++ pruebaramasfastforward/PruebaRamasFastForward.java
@@ -15,7 +15,7 @@ public class PruebaRamasFastForward {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Esto es el commit A");
+        System.out.println("Esto es el commit B");
     }
     
 }
