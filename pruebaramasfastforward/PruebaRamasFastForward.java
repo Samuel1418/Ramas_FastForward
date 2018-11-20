bd40013a74c5631b5a87a27812ef449516d6d445
Samuel1418 <sgomezpintos@danielcastelao.org>
2018-11-20 08:57:05 +0100

    Commit de primera rama

--- pruebaramasfastforward/PruebaRamasFastForward.java
+++ pruebaramasfastforward/PruebaRamasFastForward.java
@@ -15,7 +15,7 @@ public class PruebaRamasFastForward {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Esto es el commit B");
+        System.out.println("Estoy en la ptimera rama");     
     }
     
 }
